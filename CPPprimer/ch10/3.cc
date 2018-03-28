c36f48a338dcb4940f149320ceaad20ef2c41bad
me <164177565@qq.com>
2018-03-28 14:05:09 +0800

    10.2.1节完成

---
+++ CPPprimer/ch10/3.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    ifstream in(argv[1]);
+    
+    if (!in)
+    {
+        cout << "open file failed" << endl;
+        exit(1);
+    }
+
+    vector<int> vi;
+    int val, sum;
+    while (in >> val)
+        vi.push_back(val);
+
+    sum = accumulate(vi.cbegin(), vi.cend(), 0);
+    cout << sum << endl;
+
+    return 0;
+}
