bd4b3d6c927e864fde84376373ab1ad4129cfe50
hzylyq <164177565@qq.com>
2018-04-21 09:48:20 +0800

    13.1.2节完成

--- CPPprimer/ch13/HasPtr.cc
+++ CPPprimer/ch13/HasPtr.cc
@@ -4,10 +4,21 @@ class HasPtr
 {
   public:
     HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
+
     HasPtr(const HasPtr &hp)
     {
-        ps = new std::string (*hp.ps);
+        ps = new std::string(*hp.ps);
         i = hp.i;
+    }
+
+    HasPtr &operator=(const HasPtr &rhs)
+    {
+        auto newps = new std::string(*rhs.ps);
+        delete ps;
+        ps = newps;
+        i = rhs.i;
+
+        return *this;
     }
 
   private:
