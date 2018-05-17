a1545ef9315c349e75c553efdb355ccf4fc8c2cb
hzylyq <164177565@qq.com>
2018-05-17 09:19:16 +0800

    写到了2.2.6节

--- CSAPP/ch02/show_byte.c
+++ CSAPP/ch02/show_byte.c
@@ -42,15 +42,28 @@ int main(void)
     //int x = 0x12345678;
     //test_show_val(x);
 
-    int val = 0x87654321;
-    byte_pointer valp = (byte_pointer)&val;
-    show_byte(valp, 1);
-    show_byte(valp, 2);
-    show_byte(valp, 3);
+    // int val = 0x87654321;
+    // byte_pointer valp = (byte_pointer)&val;
+    // show_byte(valp, 1);
+    // show_byte(valp, 2);
+    // show_byte(valp, 3);
 
-    const char *s = "abcdef";
-    show_byte((byte_pointer)s, strlen(s));
+    // const char *s = "abcdef";
+    // show_byte((byte_pointer)s, strlen(s));
 
+    short sx = -12345;
+    unsigned short usx = sx;
+    int x = sx;
+    unsigned ux = usx;
+
+    printf("sx = %d\t", sx);
+    show_byte((byte_pointer)&sx, sizeof(short));
+    printf("usx = %u\t", usx);
+    show_byte((byte_pointer)&usx, sizeof(unsigned short));
+    printf("x = %d\t", x);
+    show_byte((byte_pointer)&x, sizeof(int));
+    printf("ux = %u\t", ux);
+    show_byte((byte_pointer)&ux, sizeof(unsigned));
 
     return 0;
 }
