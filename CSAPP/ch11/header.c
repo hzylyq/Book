384b312d2149f90f2590c7136d96eddbc3a2d75b
hzylyq <164177565@qq.com>
2018-05-25 22:58:52 +0800

    11.4 finished

--- CSAPP/ch11/header.c
+++ CSAPP/ch11/header.c
@@ -930,25 +930,25 @@ int open_clientfd(char *hostname, char *port)
     int clientfd, rc;
     struct addrinfo hints, *listp, *p;
 
-    /* Get a list of potential server addresses */
+    // Get a list of potential server addresses
     memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_socktype = SOCK_STREAM; /* Open a connection */
-    hints.ai_flags = AI_NUMERICSERV; /* ... using a numeric port arg. */
-    hints.ai_flags |= AI_ADDRCONFIG; /* Recommended for connections */
+    hints.ai_socktype = SOCK_STREAM; // Open a connection
+    hints.ai_flags = AI_NUMERICSERV; // ... using a numeric port arg.
+    hints.ai_flags |= AI_ADDRCONFIG; // Recommended for connections
     if ((rc = getaddrinfo(hostname, port, &hints, &listp)) != 0)
     {
         fprintf(stderr, "getaddrinfo failed (%s:%s): %s\n", hostname, port, gai_strerror(rc));
         return -2;
     }
 
-    /* Walk the list for one that we can successfully connect to */
+    // Walk the list for one that we can successfully connect to
     for (p = listp; p; p = p->ai_next)
     {
-        /* Create a socket descriptor */
+        // Create a socket descriptor
         if ((clientfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
-            continue; /* Socket failed, try the next */
+            continue; // Socket failed, try the next
 
-        /* Connect to the server */
+        // Connect to the server
         if (connect(clientfd, p->ai_addr, p->ai_addrlen) != -1)
             break; /* Success */
         if (close(clientfd) < 0)
