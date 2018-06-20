b0077d181ea126452add90a59a731fdf944d7d5d
hzylyq <164177565@qq.com>
2018-06-20 09:36:58 +0800

    15.2.1 finished

---
+++ CPPprimer/ch15/3.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Quote
+{
+  public:
+    Quote() = default;
+    Quote(const string &book, double sales_price) : bookNo(book), price(sales_price) {}
+
+    string isbn() const { return bookNo; }
+
+    virtual double net_price(size_t n) const
+    {
+        return n * price;
+    }
+
+    virtual ~Quote() = default;
+
+  private:
+    string bookNo;
+
+  protected:
+    double price = 0.0;
+};
+
+double print_total(ostream &os, const Quote &item, size_t n)
+{
+    double ret = item.net_price(n);
+
+    os << "isbn:" << item.isbn() << " # sold:" << n << " total due: " << ret << endl;
+
+    return ret;
+}
+
+int main(void)
+{
+    return 0;
+}
